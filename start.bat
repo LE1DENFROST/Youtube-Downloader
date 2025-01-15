eaa843bf41b9200d6fc5fead8f2a426a6016d9e2
ＳＡＶＩＴＡＲ <81708317+LE1DENFROST@users.noreply.github.com>
2025-01-15 10:50:42 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,3 @@
-REM Python'un yüklü olup olmadığını kontrol et
 python --version >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
     echo Python sistemde yüklü değil. Lütfen Python'u yükleyin.
@@ -6,7 +5,6 @@ IF %ERRORLEVEL% NEQ 0 (
     exit /b
 )
 
-REM Gerekli kütüphanelerin kurulu olup olmadığını kontrol et
 echo Gerekli kütüphaneler kontrol ediliyor...
 python -m pip show PyQt6 >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
@@ -20,9 +18,7 @@ IF %ERRORLEVEL% NEQ 0 (
     python -m pip install yt_dlp
 )
 
-REM Gerekli tüm kütüphaneler yüklendi, uygulama başlatılıyor
 echo Uygulama başlatılıyor...
 start "" pythonw tools\main.py
 
-REM CMD ekranını kapat
 exit
